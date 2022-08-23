7c8e0ed98396cf1862d0db8b7a60d88869b8ab5b
Alastair Otter <alastair@mediahack.co.za>
2022-08-23 16:17:40 +0200

    Adding trantition and loading screen

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script>
+  import { fly, fade } from 'svelte/transition'
   import { onMount } from 'svelte'
   let promise = false
-
+  let loaddata = false
   let data = []
   let dates = []
   let reports = []
@@ -42,10 +43,15 @@
         curData = data.filter((d) => d.report_date === dates[dates.length - 1])
         selected = dates[dates.length - 1]
         promise = true
+        loaddata = true
       })
   }
   function changeData() {
-    curData = data.filter((d) => d.report_date === selected)
+    loaddata = false
+    setTimeout(() => {
+      curData = data.filter((d) => d.report_date === selected)
+      loaddata = true
+    }, 500)
   }
 
   function total(curData, store) {
@@ -67,6 +73,16 @@
 
 <main>
   <h1>Grocery Basket</h1>
+
+  {#if !promise}
+    <div class="placeholder">
+      <div class="basket-icon">
+        <img src="./images/basket.svg" alt="Shopping basket icon" />
+      </div>
+      Hang on a sec, fetching the basket data...
+    </div>
+  {/if}
+
   {#if promise}
     <div class="dropdown">
       Choose a report: <select
@@ -85,23 +101,25 @@
           ><th>Report Date</th><th>Item</th><th>Checkers</th><th>Pick n Pay</th
           ><th>Spar</th><th>Woolworths</th><th>Shoprite</th></tr
         >
-        {#each curData as cd}
-          <tr
-            ><td>{cd.report_date}</td><td>{cd.type}</td><td>R{cd.checkers}</td
-            ><td>R{cd.spar}</td><td>R{cd.pick_n_pay}</td><td
-              >R{cd.woolworths}</td
-            ><td>R{cd.shoprite}</td></tr
-          >
-        {/each}
-        {#if curData}
-          <tr
-            ><td /><td />
-            <td class="total">{total(curData, 'checkers')}</td>
-            <td class="total">{total(curData, 'pick_n_pay')}</td>
-            <td class="total">{total(curData, 'spar')}</td>
-            <td class="total">{total(curData, 'woolworths')}</td>
-            <td class="total">{total(curData, 'shoprite')}</td>
-          </tr>
+        {#if loaddata}
+          {#each curData as cd}
+            <tr transition:fade={{ duration: 500 }}
+              ><td>{cd.report_date}</td><td>{cd.type}</td><td>R{cd.checkers}</td
+              ><td>R{cd.spar}</td><td>R{cd.pick_n_pay}</td><td
+                >R{cd.woolworths}</td
+              ><td>R{cd.shoprite}</td></tr
+            >
+          {/each}
+          {#if curData}
+            <tr
+              ><td /><td />
+              <td class="total">{total(curData, 'checkers')}</td>
+              <td class="total">{total(curData, 'pick_n_pay')}</td>
+              <td class="total">{total(curData, 'spar')}</td>
+              <td class="total">{total(curData, 'woolworths')}</td>
+              <td class="total">{total(curData, 'shoprite')}</td>
+            </tr>
+          {/if}
         {/if}
       </table>
     </div>
@@ -124,5 +142,17 @@
   .total {
     background: #b72b2c;
     color: #fff;
+  }
+  .placeholder {
+    margin-top: 20vh;
+    width: 100%;
+    text-align: center;
+    font-family: 'Titillium Web', Arial, Helvetica, sans-serif;
+    font-size: 1.6rem;
+    font-weight: 700;
+    color: #b72b2c;
+  }
+  .basket-icon img {
+    width: 50px;
   }
 </style>
